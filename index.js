7a60c74502d45f7f68553cc93465f2fc9b98bd09
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-06 05:32:04 +0100

    display the cards

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ let message = "";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
   sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
