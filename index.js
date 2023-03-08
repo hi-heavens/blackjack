6ba083aae210e662a906b8768b024f39bdcb4c3d
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-08 03:51:38 +0100

    generate random number and display name and chips

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-let firstCard = 10;
-let secondCard = 4;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Alvicci",
+  chips: 490
+}
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.querySelector("#cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) return 11;
+  else if (randomNumber > 10) return 10;
+  else return randomNumber;
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+
+  sum = firstCard + secondCard;
+
+  cards = [firstCard, secondCard];
+
   renderGame();
 }
 
@@ -18,8 +39,7 @@ function renderGame() {
   sumEl.textContent = "Sum: " + sum;
   cardsEl.textContent = "Cards: ";
 
-  for(let i = 0; i < cards.length; i++)
-  {
+  for (let i = 0; i < cards.length; i++) {
     cardsEl.textContent += cards[i] + " ";
   }
 
@@ -36,9 +56,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-  let newCard = 7;
-  sum += newCard;
-  cards.push(newCard);
-  renderGame();
+  if (isAlive && hasBlackJack == false) {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+  }
 }
